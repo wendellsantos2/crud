ac9cccc11bb23756dca665f566850f1e78b85d23
wendellsantos2 <wemdellcos@gmail.com>
2023-04-05 09:55:37 -0300

    Inicio do projeto

---
+++ src/pages/usuario/main/index.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react ';
+import {Link} from 'react-router-dom';
+import './index.css';
+
+
+export default class Main extends Component {
+    constructor(){
+        this.state = {
+            usuario: []
+        }
+    }
+    componentDidMount(){
+        fetch()
+    }
+}
